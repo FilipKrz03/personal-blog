a039e69a0de629cea5b3c6ed1da4334ea94583fd
Filip Krzemień <filipos2003@gmail.com>
2023-06-03 00:49:36 +0200

    Added working nav links

--- app/posts/components/Header.tsx
+++ app/posts/components/Header.tsx
@@ -1,15 +1,32 @@
 import Link from "next/link";
+import { NavLinks } from "@/app/types/NavLinks";
+import { usePathname } from 'next/navigation';;
 import classes from "./Header.module.scss";
 
+
 const Header = () => {
+
+  const pathname = usePathname();
+
+  const navLinks:NavLinks = [
+    {title:'Home' , path:'/'} , 
+    {title:'My Posts' , path:'/posts'} , 
+    {title:'Technology' , path:'/technology'} , 
+    {title:'Funny' , path:'/funny'} , 
+    {title:'Curiosites' , path:'/couriosites'} , 
+  ]
+  
+
   return (
     <header className={classes.header}>
         <ul>
-          <Link href="/"><li>Home</li></Link>
-          <li>My Posts</li>
-          <li>Technology</li>
-          <li>Funny</li>
-          <li>Curiosites</li>
+        {navLinks.map(link => {
+          return(
+           <li key={link.title} className={link.path === pathname ? classes.active : ''}>
+            <Link href={link.path}>{link.title}</Link>
+           </li>
+          )
+        })}
         </ul>
     </header>
   );
