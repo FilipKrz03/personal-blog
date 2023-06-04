b9a59d8192c51587560d39ae3b914014da5c277d
Filip Krzemień <filipos2003@gmail.com>
2023-06-04 22:00:54 +0200

    restructurization of posts page

---
+++ app/posts/components/StartAnimation.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+import Lottie , {LottieRefCurrentProps} from "lottie-react";
+import welcome from '../../../public/welcome.json';
+
+const StartAnimation = () => {
+
+    const welcomeRef = useRef<LottieRefCurrentProps>(null);
+
+    return (
+    <motion.div animate={{y:-30}} initial={{y:-340}} transition={{duration:0.2}}>
+    <Lottie lottieRef={welcomeRef} onComplete={()=>{
+      welcomeRef.current?.destroy()
+    }} animationData={welcome} style={{width:'250px' , zIndex:20}} loop={false}  />
+  </motion.div>
+    )
+
+}
+
+export default StartAnimation;
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,26 +1,20 @@
-"use client";
-import React , {useRef} from 'react'
+import  {useRef} from 'react'
+import StartAnimation from './components/StartAnimation';
 import getMainPost from '@/lib/getMainPost';
 import getMainSmallPosts from '@/lib/getMainSmallPosts';
-import Lottie , {LottieRefCurrentProps} from "lottie-react";
-import welcome from '../../public/welcome.json';
-import { motion } from "framer-motion";
+
 import classes from './page.module.scss';
 
 
 export default async function MyPosts() {
 
-  const welcomeRef = useRef<LottieRefCurrentProps>(null);
+
   const data = await getMainSmallPosts();
   console.log(data);
 
   return (
     <div className={classes.page}>
-    <motion.div animate={{y:-30}} initial={{y:-340}} transition={{duration:0.2}} className={classes.animation}>
-      <Lottie lottieRef={welcomeRef} onComplete={()=>{
-        welcomeRef.current?.destroy()
-      }} animationData={welcome} style={{width:'250px' , zIndex:20}} loop={false}  />
-    </motion.div>
+      <StartAnimation />
     </div>
   )
 }
