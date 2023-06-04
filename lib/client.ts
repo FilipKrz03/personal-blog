fe28289492804eae0524793b9608cbb9f2351dcc
Filip Krzemień <filipos2003@gmail.com>
2023-06-04 20:50:51 +0200

    Added contentull page

---
+++ lib/client.ts
@@ -0,0 +1,5 @@
+const contentful = require('contentful')
+export const client = contentful.createClient({
+  space: '7wohefq1rwua',
+  accessToken: 'S3Cp3sM1npP_CUqea-qXPDnhgb5e0wjW_JVtK4AD8Ec',
+})
