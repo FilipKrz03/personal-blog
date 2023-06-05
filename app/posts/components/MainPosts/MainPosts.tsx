bfa2dcd8dc0f64a386b49c03dba37e815390d5ac
Filip Krzemień <filipos2003@gmail.com>
2023-06-05 16:24:16 +0200

    Added stylyles

--- app/posts/components/MainPosts/MainPosts.tsx
+++ app/posts/components/MainPosts/MainPosts.tsx
@@ -16,19 +16,19 @@ type Params = {
 const MainPosts  = ({params : {mainPost , mainSmallPosts}}:Params) => {
     return(
         <div  className={classes.post}>
-          <motion.div  whileHover={{scale:1.1}} className={classes.left}>
+          <motion.div  whileHover={{scale:1.1 ,}} className={classes.left}>
             <img src={"http:" + mainPost.image.url} alt={mainPost.image.title} />
             <p>{mainPost.fields.title}</p>
           </motion.div>
           <div className={classes.right}>
-            <div className={classes['right-item']}>
+            <motion.div className={classes['right-item']} whileHover={{scale:1.1}}>
                 <img src={"http:" + mainSmallPosts[0].image.url} alt={mainPost.image.title} />
                 <p>{mainSmallPosts[0].fields.title}</p>
-            </div>
-            <div className={classes['right-item']}>
+            </motion.div>
+            <motion.div className={classes['right-item']} whileHover={{scale:1.1}}>
                 <img src={"http:" + mainSmallPosts[1].image.url} alt={mainPost.image.title} />
                 <p>{mainSmallPosts[1].fields.title}</p>
-            </div>
+            </motion.div>
             </div>
         </div>
     )
