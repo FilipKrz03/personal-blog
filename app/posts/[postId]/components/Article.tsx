378abe789e9107e36144672a90229b681febecc1
Filip Krzemień <filipos2003@gmail.com>
2023-06-06 11:59:03 +0200

    Improved post detail styling

--- app/posts/[postId]/components/Article.tsx
+++ app/posts/[postId]/components/Article.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 import { PostData } from '@/app/types/PostData';
 import classes from './Article.module.scss';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useRouter } from 'next/navigation';
 
 type postData = {
     data:PostData , 
@@ -8,12 +11,18 @@ type postData = {
 
 const Article = ({data}:postData) => {
 
+    const router = useRouter();
+
     return(
         <article className={classes.article}>
+            <ArrowBackIcon fontSize='large'
+              style={{position:'absolute' , left:50 , color:'white' , cursor:'pointer'}}
+              onClick = {()=>{router.push('/posts')}}
+              />
             <img src={data.image.url} alt={data.image.title} />
             <h2>{data.fields.title}</h2>
             <p className={classes.desc}>{data.fields.shortDescription}</p>
-            <p>{data.fields.article}</p>
+           <ReactMarkdown>{data.fields.article}</ReactMarkdown>
         </article>
     )
 
