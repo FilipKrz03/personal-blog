c6c0cabe97acec9eb52c9466fc9a71874f7c7848
Filip Krzemień <filipos2003@gmail.com>
2023-06-06 22:33:40 +0200

    Basic error handling

---
+++ app/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Header from "./components/Header";
+
+export default function Error({error , reset}:{error:Error , reset: () => void}) {
+
+
+  return (
+    <>
+        <Header />
+        <p style={{textAlign:'center' , marginTop:'100px'}}>Something went wrong</p>
+    </>
+  )
+}
+
--- lib/getMainPost.ts
+++ lib/getMainPost.ts
@@ -4,6 +4,9 @@ import { PostData } from '@/app/types/PostData';
 const getMainPost = async() =>{
 
     const request = await client.getEntries({content_type: 'myBlogMainLg'});
+    if(!request.items){
+        throw new Error('Failed to fetch data !');
+    }
     const data:PostData = 
     {
     fields:request.items[0].fields ,
