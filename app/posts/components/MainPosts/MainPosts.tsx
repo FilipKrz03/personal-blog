ed51196b25aa623dd82530b89d1834284b79ce20
Filip Krzemień <filipos2003@gmail.com>
2023-06-07 17:43:27 +0200

    Addes main post page navigation

--- app/posts/components/MainPosts/MainPosts.tsx
+++ app/posts/components/MainPosts/MainPosts.tsx
@@ -19,61 +19,68 @@ const MainPosts = ({ params: { mainPost, mainSmallPosts } }: Params) => {
   return (
     <div className={classes.post}>
       <motion.div
-        whileHover={{ scale: 1.1 }}
-        className={classes.left}
-        id={mainPost.id}
-        onClick={() => {
-          router.push("posts/" + mainPost.id);
-        }}
+        className={classes.posts}
+        animate={{ scale: 1 , filter: "blur(0px)"  }}
+        initial={{ scale: 0 , filter: "blur(4px)"}}
+        transition={{ duration: 0.5, delay: 0.25 , filter:{delay:0.7 , duration:0.3} }}
       >
-        <Image
-          src={"http:" + mainPost.image.url}
-          alt={mainPost.image.title}
-          width={0}
-          height={0}
-          sizes="37vw"
-          style={{ aspectRatio: "1/0.975", width: "37vw", height: "auto" }}
-        />
-        <p>{mainPost.fields.title}</p>
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          className={classes.left}
+          id={mainPost.id}
+          onClick={() => {
+            router.push("posts/" + mainPost.id);
+          }}
+        >
+          <Image
+            src={"http:" + mainPost.image.url}
+            alt={mainPost.image.title}
+            width={0}
+            height={0}
+            sizes="37vw"
+            style={{ aspectRatio: "1/0.975", width: "37vw", height: "auto" }}
+          />
+          <p>{mainPost.fields.title}</p>
+        </motion.div>
+        <div className={classes.right}>
+          <motion.div
+            className={classes["right-item"]}
+            whileHover={{ scale: 1.1 }}
+            id={mainSmallPosts[0].id}
+            onClick={() => {
+              router.push("posts/" + mainSmallPosts[0].id);
+            }}
+          >
+            <Image
+              src={"http:" + mainSmallPosts[0].image.url}
+              alt={mainPost.image.title}
+              width={0}
+              height={0}
+              sizes="35vw"
+              style={{ aspectRatio: "2.4/1", width: "35vw", height: "auto" }}
+            />
+            <p>{mainSmallPosts[0].fields.title}</p>
+          </motion.div>
+          <motion.div
+            className={classes["right-item"]}
+            whileHover={{ scale: 1.1 }}
+            id={mainSmallPosts[1].id}
+            onClick={() => {
+              router.push("posts/" + mainSmallPosts[1].id);
+            }}
+          >
+            <Image
+              src={"http:" + mainSmallPosts[1].image.url}
+              alt={mainPost.image.title}
+              width={0}
+              height={0}
+              sizes="35vw"
+              style={{ aspectRatio: "2.4/1", width: "36vw", height: "auto" }}
+            />
+            <p>{mainSmallPosts[1].fields.title}</p>
+          </motion.div>
+        </div>
       </motion.div>
-      <div className={classes.right}>
-        <motion.div
-          className={classes["right-item"]}
-          whileHover={{ scale: 1.1 }}
-          id={mainSmallPosts[0].id}
-          onClick={() => {
-            router.push("posts/" + mainSmallPosts[0].id);
-          }}
-        >
-          <Image
-            src={"http:" + mainSmallPosts[0].image.url}
-            alt={mainPost.image.title}
-            width={0}
-            height={0}
-            sizes="35vw"
-            style={{aspectRatio:'2.4/1' , width:'35vw' , height:'auto'}}
-          />
-          <p>{mainSmallPosts[0].fields.title}</p>
-        </motion.div>
-        <motion.div
-          className={classes["right-item"]}
-          whileHover={{ scale: 1.1 }}
-          id={mainSmallPosts[1].id}
-          onClick={() => {
-            router.push("posts/" + mainSmallPosts[1].id);
-          }}
-        >
-          <Image
-            src={"http:" + mainSmallPosts[1].image.url}
-            alt={mainPost.image.title}
-            width={0}
-            height={0}
-            sizes="35vw"
-            style={{aspectRatio:'2.4/1' , width:'36vw' , height:'auto'}}
-          />
-          <p>{mainSmallPosts[1].fields.title}</p>
-        </motion.div>
-      </div>
     </div>
   );
 };
