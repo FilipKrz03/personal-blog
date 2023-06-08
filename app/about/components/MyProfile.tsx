2d5a44125b75c063c8676c8320398cf22a482cc9
Filip Krzemień <filipos2003@gmail.com>
2023-06-08 22:58:32 +0200

    Imporved about section

--- app/about/components/MyProfile.tsx
+++ app/about/components/MyProfile.tsx
@@ -8,54 +8,63 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import myImage from "../../../public/img/[removal.ai]_tmp-64822d70be201_FDQPM6.png";
 import classes from "./MyProfile.module.scss";
 import { motion } from "framer-motion";
+import { TypeAnimation } from "react-type-animation";
 
 const MyProfile = () => {
   return (
     <div className={classes.info}>
         <p>Hello ! 🖐️ if you are like my site you can visit my medias , especially i invite to visit my GitHub to see my projects ! </p>
       <div className={classes.icons}>
-        <motion.div
-          animate={{ scale: 1 }}
-          initial={{ scale: 0 , }}
-          transition={{ delay: 0.1 }}
-  
-        >
-          <Link href={"https://www.facebook.com/"}>
-            <FacebookIcon fontSize="large" />
-          </Link>
-        </motion.div>
-        <motion.div
-          animate={{ scale: 1  }}
+      <motion.div
+          animate={{ scale: 1.5 }}
           initial={{ scale: 0 }}
           transition={{ delay: 0.3 }}
-          whileHover={{scale:1.3 , opacity:0.9}}
         >
-          <Link href={"https://www.facebook.com/"}>
-            <InstagramIcon fontSize="large" />
+          <Link href={"https://github.com/FilipKrz03"}>
+            <GitHubIcon fontSize="large" color="secondary" />
           </Link>
         </motion.div>
         <motion.div
-          animate={{ scale: 1 }}
-          initial={{ scale: 0 }}
+          animate={{ scale: 1.5 }}
+          initial={{ scale: 0 , }}
           transition={{ delay: 0.5 }}
-          whileHover={{scale:1.3 , opacity:0.9}}
         >
           <Link href={"https://www.facebook.com/"}>
-            <GitHubIcon fontSize="large" />
+            <FacebookIcon fontSize="large" color="secondary" />
           </Link>
         </motion.div>
         <motion.div
-          animate={{ scale: 1}}
+          animate={{ scale: 1.5  }}
           initial={{ scale: 0 }}
           transition={{ delay: 0.7 }}
-          whileHover={{scale:1.3 , opacity:0.9}}
         >
           <Link href={"https://www.facebook.com/"}>
-            <TwitterIcon fontSize="large" />
+            <InstagramIcon fontSize="large" color="secondary" />
+          </Link>
+        </motion.div>
+        <motion.div
+          animate={{ scale: 1.5}}
+          initial={{ scale: 0 }}
+          transition={{ delay: 0.9 }}
+          
+        >
+          <Link href={"https://www.facebook.com/"}>
+            <TwitterIcon fontSize="large" color="secondary" />
           </Link>
         </motion.div>
       </div>
+      <div className={classes.more}>
       <Image src={myImage} width={300} height={400} alt="My Image" />
+      <TypeAnimation
+      className={classes.gradient}
+      speed={25}
+      style={{ whiteSpace: 'pre-line', height: '360px', display: 'block' , width:'300px' , fontSize:'50px',}}
+      sequence={[
+        `My Skills:\n` , 3000 , 
+        `HTML\nCSS/SCSS\nJavascript\nReact/Redux\nTypescript\nNext.js`
+      ]}
+      />
+      </div>
     </div>
   );
 };
