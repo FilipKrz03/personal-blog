59e9b27283cf6f210b01060ef1e9ce8107989456
Filip Krzemień <filipos2003@gmail.com>
2023-06-09 21:51:20 +0200

    Added

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,11 +1,10 @@
 import getSinglePost from "@/lib/getSinglePost";
-import {Suspense} from 'react';
 import { PostData } from "@/app/types/PostData";
 import Article from "./components/Article";
 import getMainSmallPosts from "@/lib/getMainSmallPosts";
 import getMainPost from "@/lib/getMainPost";
 import getOtherPosts from "@/lib/getOtherPosts";
-import LoadingState from "@/app/components/LoadingState";
+
 
 type Params = {
   params: {
