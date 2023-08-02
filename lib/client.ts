337f6dd29a0ef02322520ba6525b02966ebe1567
FilipKrz03 <122054858+FilipKrz03@users.noreply.github.com>
2023-08-03 01:07:23 +0200

    Update client.ts

--- lib/client.ts
+++ lib/client.ts
@@ -1,5 +1,5 @@
 const contentful = require("contentful");
 export const client = contentful.createClient({
-  space: "7wohefq1rwua",
-  accessToken: "S3Cp3sM1npP_CUqea-qXPDnhgb5e0wjW_JVtK4AD8Ec",
+  space: process.env
+  accessToken: process.env
 });
